342b92d685ef4587b967fc5b29d5767d65ca8fab
tremisabdoul <75373856+tremisabdoul@users.noreply.github.com>
2021-08-10 12:42:47 +0200

    upOublie.sorry

--- Scripts/Personnages/Archer/Arrow.gd
+++ Scripts/Personnages/Archer/Arrow.gd
@@ -3,33 +3,36 @@ extends KinematicBody2D
 """"""
 
 onready var NodeAnimation = $Animations
-onready var Time = $LifeTime
 onready var Hitbox = $Area
 
 var launcher = []
-var velocity = Vector2()
+var velocity = Vector2(1, 0)
 var speed = 15
 var End = 0
 var Damage = 100
+
 """"""
 
-func Movement(delta):
-	velocity = (velocity.normalized().rotated(rotation) * speed) * (delta * 60)
-	velocity = move_and_collide(velocity)
-	if Hitbox.get_overlapping_bodies():
-		End = Hitbox.get_overlapping_bodies()[0]
-	velocity = Vector2()
 
 func Launch(LauncherPosition: Vector2, LauncherDirection, _Damage, CollisionLayer):
 	visible = false
 	position = LauncherPosition
 	rotation_degrees = LauncherDirection
 	Damage = _Damage
-	velocity = (velocity.normalized().rotated(rotation) * speed)
 	collision_mask = CollisionLayer
 	Hitbox.collision_mask = CollisionLayer
 	Hitbox.set_collision_layer_bit(20, false)
-	
+	velocity = Vector2(speed, 0)
+
+func _physics_process(delta):
+	velocity = velocity.normalized().rotated(rotation) * (delta * 60)
+
+	var collision = move_and_collide(velocity)
+	if collision:
+		rotation = velocity.angle()
+		if collision.collider.has_method("hit"):
+			collision.collider.hit()
+
 func _on_Area2D_body_entered(body):
 	print('uwuwowo')
 	
@@ -39,9 +42,11 @@ func EndAnimWaiter():
 	queue_free()
 
 func _process(delta):
-	if End or not Time.get_time_left():
+	if End:
 		EndAnimWaiter()
 	else:
 		visible = true
-		velocity.x = 1
-		Movement(delta)
+
+
+func _on_Area_area_entered(area):
+	End = Hitbox.get_overlapping_bodies()[0]
