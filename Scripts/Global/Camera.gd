6dbda8172510b427a26a78ea56fdedaf7febd9f3
tremisabdoul <75373856+tremisabdoul@users.noreply.github.com>
2021-09-08 14:16:33 +0200

    Bloom plus barres

--- Scripts/Global/Camera.gd
+++ Scripts/Global/Camera.gd
@@ -77,13 +77,45 @@ func _process(delta):
 	position = (CameraPosition + lastPos * 10) / 11
 	UI.set_position((position - lastPos) * 5)
 	P1Hp.set_size(Vector2((Player1.Hp * 500) / Player1.MaxHp, 1))
+	
 	P1Q.set_size(Vector2(((((Player1.QCD - Player1.Q) / Player1.QCD) + (P1Q.get_size()[0]) / 136) / 2) * 136, 1))
+	if Player1.Q <= 0:
+		P1Q.get_parent().self_modulate = Color(0.8, 0.8, 0.8, 0.5)
+	else:
+		P1Q.get_parent().self_modulate = Color(0.2, 0.2, 0.2, 0.2)
+	
 	P1W.set_size(Vector2(((((Player1.WCD - Player1.W) / Player1.WCD) + (P1W.get_size()[0]) / 136) / 2) * 136, 1))
+	if Player1.W <= 0:
+		P1W.get_parent().self_modulate = Color(0.8, 0.8, 0.8, 0.5)
+	else:
+		P1W.get_parent().self_modulate = Color(0.2, 0.2, 0.2, 0.2)
+	
 	P1E.set_size(Vector2(((((Player1.ECD - Player1.E) / Player1.ECD) + (P1E.get_size()[0]) / 136) / 2) * 136, 1))
+	if Player1.E <= 0:
+		P1E.get_parent().self_modulate = Color(0.8, 0.8, 0.8, 0.5)
+	else:
+		P1E.get_parent().self_modulate = Color(0.2, 0.2, 0.2, 0.2)
+	
 	P2Hp.set_size(Vector2((Player2.Hp * 500) / Player2.MaxHp, 1))
+	
 	P2Q.set_size(Vector2(((((Player2.QCD - Player2.Q) / Player2.QCD) + (P2Q.get_size()[0]) / 136) / 2) * 136, 1))
+	if Player2.Q <= 0:
+		P2Q.get_parent().self_modulate = Color(0.8, 0.8, 0.8, 0.5)
+	else:
+		P2Q.get_parent().self_modulate = Color(0.2, 0.2, 0.2, 0.2)
+	
 	P2W.set_size(Vector2(((((Player2.WCD - Player2.W) / Player2.WCD) + (P2W.get_size()[0]) / 136) / 2) * 136, 1))
+	if Player2.W <= 0:
+		P2W.get_parent().self_modulate = Color(0.8, 0.8, 0.8, 0.5)
+	else:
+		P2W.get_parent().self_modulate = Color(0.2, 0.2, 0.2, 0.2)
+	
 	P2E.set_size(Vector2(((((Player2.ECD - Player2.E) / Player2.ECD) + (P2E.get_size()[0]) / 136) / 2) * 136, 1))
+	if Player2.E <= 0:
+		P2E.get_parent().self_modulate = Color(0.8, 0.8, 0.8, 0.5)
+	else:
+		P2E.get_parent().self_modulate = Color(0.2, 0.2, 0.2, 0.2)
+	
 	lastPos = position
 	lastZoom = zoom.x
 	rotation = Rotate
